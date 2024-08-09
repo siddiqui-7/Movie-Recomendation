40e9d75efb05ce33930ef58cbfae7ef50b420025
Umer Siddiqui <155855762+siddiqui-7@users.noreply.github.com>
2024-08-09 19:49:07 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=9ea861012cb8683e6bb7a022db8d8674".format(movie_id)
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/original" + poster_path
+        return full_path
+    else:
+        return None  # Handle the case where there's no poster path
+
+movies = pickle.load(open("movies_list.pkl", 'rb'))
+similarity = pickle.load(open("similarity.pkl", 'rb'))
+movies_list = movies['title'].values
+
+st.header("Movie Recommender System")
+
+import streamlit.components.v1 as components
+
+imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
+
+imageUrls = [
+    fetch_poster(1632),
+    fetch_poster(299536),
+    fetch_poster(17455),
+    fetch_poster(2830),
+    fetch_poster(429422),
+    fetch_poster(9722),
+    fetch_poster(13972),
+    fetch_poster(240),
+    fetch_poster(155),
+    fetch_poster(598),
+    fetch_poster(914),
+    fetch_poster(255709),
+    fetch_poster(572154)
+]
+
+# Filter out None values in imageUrls
+imageUrls = [url for url in imageUrls if url]
+
+imageCarouselComponent(imageUrls=imageUrls, height=200)
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
+    for i in distance[1:6]:
+        movie_id = movies.iloc[i[0]].id
+        recommend_movie.append(movies.iloc[i[0]].title)
+        recommend_poster.append(fetch_poster(movie_id))
+    return recommend_movie, recommend_poster
+
+if st.button("Show Recommend"):
+    movie_name, movie_poster = recommend(selectvalue)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+        st.image(movie_poster[0])
+    with col2:
+        st.text(movie_name[1])
+        st.image(movie_poster[1])
+    with col3:
+        st.text(movie_name[2])
+        st.image(movie_poster[2])
+    with col4:
+        st.text(movie_name[3])
+        st.image(movie_poster[3])
+    with col5:
+        st.text(movie_name[4])
+        st.image(movie_poster[4])
